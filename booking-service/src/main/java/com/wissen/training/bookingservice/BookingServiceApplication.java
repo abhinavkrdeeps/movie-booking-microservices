f6875624b000a8e9e1a12f3633870aacbfabefcb
abhinavkrdeeps <abhikrdips@gmail.com>
2021-11-06 08:03:03 +0530

    Added Eureka CLient and load balancing

--- booking-service/src/main/java/com/wissen/training/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/wissen/training/bookingservice/BookingServiceApplication.java
@@ -2,13 +2,29 @@ package com.wissen.training.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class BookingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getClient() {
+		return WebClient.builder();
 	}
 
 }
