96e92235d4b67d281e2966dc6cf6821996f9dafe
abhinavkrdeeps <abhikrdips@gmail.com>
2021-11-07 16:07:45 +0530

    Added spring cloud config server

---
+++ centralized-config-server/src/test/java/com/wissen/training/centralizedconfigserver/CentralizedConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wissen.training.centralizedconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
